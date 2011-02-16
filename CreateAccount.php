6753172bb0da4359cb315385cf4aa8de19a1bf7a
crownandthering@gmail.com <crownandthering@gmail.com@2cb8fbac-058a-50cf-f547-5787fd4e6422>
2011-02-16 13:53:12 +0000

    Up and running again. But problems with the remember button!

--- CreateAccount.php
+++ CreateAccount.php
@@ -1,11 +1,10 @@
 <?php
 require_once("lib/LoadSystem.php");
-global $TEXT; 
 ?> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
-		<title><?php echo $GLOBALS['CompanyName'];/*." ".$TEXT['Manager']*/?></title>
+		<title><?php echo COMPANY_NAME;/*." ".$TEXT['Manager']*/?></title>
 		<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 	</head>
