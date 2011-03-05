af17cadd05ae921a4f4a831be98b190ba81ec47d
antonov_g@abv.bg <antonov_g@abv.bg@2cb8fbac-058a-50cf-f547-5787fd4e6422>
2011-03-05 22:27:28 +0000

    ]

--- modules/Menu/menu.php
+++ modules/Menu/menu.php
@@ -2,67 +2,77 @@
     <?php
     if(isset($_SESSION['LoggedIn']))
     { ?>
-    <li class="level_1">
-        <a href="index.php?page=Tasks"><?php echo TASKS_TEXT; ?></a>
-    </li>
-    <li class="level_1">
-        <a href=# onmouseover="openLevel_2('users_submenu')" onmouseout="closeLevel_2AfterTime()"><?php echo USERS_TEXT; ?></a>
-        <ul class="level_2" id="users_submenu" onmouseover="cancelLevel_2Timeout()" onmouseout="closeLevel_2AfterTime()">
-            <li class="level_2">
-                <a href=# onmouseover="openLevel_3('all_users')" onmouseout="closeLevel_3AfterTime()"><?php echo VIEW_ALL_TEXT; ?></a>
-                <ul class="level_3" id="all_users" onmouseover="cancelLevel_3Timeout()" onmouseout="closeLevel_3AfterTime()">
-                    <li class="level_3"><a href="index.php?page=Employees"><?php echo EMPLOYEES_TEXT; ?></a></li>
-                    <li class="level_3 last"><a href="index.php?page=Clients"><?php echo CLIENTS_TEXT; ?></a></li>
+        <li class="level_1">
+            <a href="index.php?page=Tasks"><?php echo TASKS_TEXT; ?></a>
+        </li>
+        <?php
+        if($_SESSION['userinfo']['EmployeeOrClient']=='e')
+        { ?>
+            <li class="level_1">
+                <a href=# onmouseover="openLevel_2('users_submenu')" onmouseout="closeLevel_2AfterTime()"><?php echo USERS_TEXT; ?></a>
+                <ul class="level_2" id="users_submenu" onmouseover="cancelLevel_2Timeout()" onmouseout="closeLevel_2AfterTime()">
+                    <li class="level_2">
+                        <a href=# onmouseover="openLevel_3('all_users')" onmouseout="closeLevel_3AfterTime()"><?php echo VIEW_ALL_TEXT; ?></a>
+                        <ul class="level_3" id="all_users" onmouseover="cancelLevel_3Timeout()" onmouseout="closeLevel_3AfterTime()">
+                            <li class="level_3"><a href="index.php?page=Employees"><?php echo EMPLOYEES_TEXT; ?></a></li>
+                            <li class="level_3 last"><a href="index.php?page=Clients"><?php echo CLIENTS_TEXT; ?></a></li>
+                        </ul>
+                    </li>
+                    <?php if($_SESSION['userinfo']['CanCreateAccounts']!='n')
+                    { ?>
+                        <li class="level_2 last">
+                            <a href="index.php?page=CreateAccount"><?php echo CREATE_ACCOUNT_TEXT; ?></a>
+                        </li>
+                    <?php }?>
                 </ul>
             </li>
-            <li class="level_2 last">
-                <a href="index.php?page=CreateAccount"><?php echo CREATE_ACCOUNT_TEXT; ?></a>
-            </li>
-        </ul>
-    </li>
-    <li class="level_1">
-        <a href=# onmouseover="openLevel_2('manage_submenu')" onmouseout="closeLevel_2AfterTime()"><?php echo MANAGE_TEXT; ?></a>
-        <ul class="level_2" id="manage_submenu" onmouseover="cancelLevel_2Timeout()" onmouseout="closeLevel_2AfterTime()">
-            <li class="level_2">
-                <a href="index.php?page=Branches" onmouseover="openLevel_3('branches')" onmouseout="closeLevel_3AfterTime()"><?php echo BRANCHES_TEXT; ?></a>
-                <ul class="level_3" id="branches" onmouseover="cancelLevel_3Timeout()" onmouseout="closeLevel_3AfterTime()">
-                    <li class="level_3">
-                        <a href="index.php?page=EditBranches"><?php echo ADD_TEXT; ?></a>
-                    </li>
-                    <li class="level_3 last">
-                        <a href="index.php?page=Branches"><?php echo VIEW_ALL_TEXT; ?></a>
+            <li class="level_1">
+                <a href=# onmouseover="openLevel_2('manage_submenu')" onmouseout="closeLevel_2AfterTime()"><?php echo MANAGE_TEXT; ?></a>
+                <ul class="level_2" id="manage_submenu" onmouseover="cancelLevel_2Timeout()" onmouseout="closeLevel_2AfterTime()">
+                <?php if($_SESSION['userinfo']['IsAdmin'])
+                    { ?>
+                        <li class="level_2">
+                            <a href="index.php?page=Branches" onmouseover="openLevel_3('branches')" onmouseout="closeLevel_3AfterTime()"><?php echo BRANCHES_TEXT; ?></a>
+                            <ul class="level_3" id="branches" onmouseover="cancelLevel_3Timeout()" onmouseout="closeLevel_3AfterTime()">
+                                <li class="level_3">
+                                    <a href="index.php?page=EditBranches"><?php echo ADD_TEXT; ?></a>
+                                </li>
+                                <li class="level_3 last">
+                                    <a href="index.php?page=Branches"><?php echo VIEW_ALL_TEXT; ?></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="level_2">
+                            <a href="index.php?page=EditTitles"><?php echo TITLES1_TEXT; ?></a>
+                        </li>
+                        <li class="level_2">
+                            <a href="index.php?page=EditPositions"><?php echo POSITIONS1_TEXT; ?></a>
+                        </li>
+                    <?php }?>
+                    <li class="level_2 last">
+                        <a href="index.php?page=Projects" onmouseover="openLevel_3('projects')" onmouseout="closeLevel_3AfterTime()"><?php echo PROJECTS_TEXT; ?></a>
+                        <ul class="level_3" id="projects" onmouseover="cancelLevel_3Timeout()" onmouseout="closeLevel_3AfterTime()">
+                            <li class="level_3">
+                                <a href="index.php?page=EditProject"><?php echo ADD_TEXT; ?></a>
+                            </li>
+                            <li class="level_3 last">
+                                <a href="index.php?page=Projects"><?php echo VIEW_ALL_TEXT; ?></a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </li>
-            <li class="level_2">
-                <a href="index.php?page=EditTitles"><?php echo TITLES1_TEXT; ?></a>
-            </li>
-            <li class="level_2">
-                <a href="index.php?page=EditPositions"><?php echo POSITIONS1_TEXT; ?></a>
-            </li>
-            <li class="level_2 last">
-                <a href="index.php?page=Projects" onmouseover="openLevel_3('projects')" onmouseout="closeLevel_3AfterTime()"><?php echo PROJECTS_TEXT; ?></a>
-                <ul class="level_3" id="projects" onmouseover="cancelLevel_3Timeout()" onmouseout="closeLevel_3AfterTime()">
-                    <li class="level_3">
-                        <a href="index.php?page=EditProject"><?php echo ADD_TEXT; ?></a>
-                    </li>
-                    <li class="level_3 last">
-                        <a href="index.php?page=Projects"><?php echo VIEW_ALL_TEXT; ?></a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </li>
-    <li class="level_1">
-        <a href=# onmouseover="openLevel_2('account_submenu')" onmouseout="closeLevel_2AfterTime()"><?php echo ACCOUNT_TEXT; ?></a>
-        <ul class="level_2" id="account_submenu" onmouseover="cancelLevel_2Timeout()" onmouseout="closeLevel_2AfterTime()">
-            <li class="level_2">
-                <a href="index.php?page=ChangePassword"><?php echo CHANGE_PASSWORG_TEXT; ?></a>
-            </li>
-            <li class="level_2 last">
-                <a href=#><?php echo EDIT_TEXT; ?></a>
-            </li>
-        </ul>
-    </li>
+        <?php }?>
+        <li class="level_1">
+            <a href=# onmouseover="openLevel_2('account_submenu')" onmouseout="closeLevel_2AfterTime()"><?php echo ACCOUNT_TEXT; ?></a>
+            <ul class="level_2" id="account_submenu" onmouseover="cancelLevel_2Timeout()" onmouseout="closeLevel_2AfterTime()">
+                <li class="level_2">
+                    <a href="index.php?page=ChangePassword"><?php echo CHANGE_PASSWORG_TEXT; ?></a>
+                </li>
+                <li class="level_2 last">
+                    <a href="index.php?page=EditMyAccount"><?php echo EDIT_TEXT; ?></a>
+                </li>
+            </ul>
+        </li>
     <?php }?>
 </ul>
